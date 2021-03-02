4e5e4d40a0cb0e5864bab5f04516eab8008ad4d5
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-03-02 22:10:48 +1300

    String conversion functions
    Add functions for converting strings to vectors of numbers, and
    vice versa.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use regex;
 use std::fs; // For reading input files
 
+const LETTERS: [char; 26] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
 /// Read and preprocess input
 ///
 /// Accepts a file path or string literal and returns an uppercased `String` containing the file
@@ -30,10 +35,42 @@ fn read_input(input: &str) -> String {
     re.replace_all(&res, "").to_uppercase()
 }
 
+fn char_to_num(c: char) -> usize {
+    LETTERS.iter().position(|&x| x == c).unwrap()
+}
+
+fn num_to_char(n: usize) -> char {
+    LETTERS[n]
+}
+
+fn string_to_vec(s: String) -> Vec<char> {
+    s.chars().collect::<Vec<char>>()
+}
+
+fn vec_to_string(v: Vec<char>) -> String {
+    v.iter().collect()
+}
+
+fn string_to_nums(s: String) -> Vec<usize> {
+    string_to_vec(s)
+        .into_iter()
+        .map(|x| char_to_num(x))
+        .collect::<Vec<usize>>()
+}
+
+fn nums_to_string(v: Vec<usize>) -> String {
+    v.into_iter().map(|x| num_to_char(x)).collect::<String>()
+}
+
 fn main() {
-    let in_file = "poem.txts";
-    let in_text = read_input(in_file);
+    // let in_file = "poem.txt";
+    // let in_text = read_input(in_file);
+    // println!("Key:\n{}", in_text);
+    // let in_nums = string_to_nums(in_text);
+    // println!("Key nums:\n{:?}", in_nums);
     let key_file = "key.txt";
     let key_text = read_input(key_file);
-    println!("KEY:\n{}\nINPUT:\n{}", key_text, in_text);
+    println!("Key:\n{}", key_text);
+    let key_nums = string_to_nums(key_text);
+    println!("Key nums:\n{:?}", key_nums);
 }
