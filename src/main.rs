f29bab5f1b2da3fea6523fc804c59386a5806268
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-03-06 15:26:12 +1300

    Finish prototype

--- src/main.rs
+++ src/main.rs
@@ -35,59 +35,173 @@ fn read_input(input: &str) -> String {
     re.replace_all(&res, "").to_uppercase()
 }
 
+/// Convert a character into a numeric position in the alphabet
 fn char_to_num(c: char) -> usize {
     LETTERS.iter().position(|&x| x == c).unwrap() + 1
 }
 
+#[test]
+fn test_char_to_num() {
+    assert_eq!(char_to_num('A'), 1);
+    assert_eq!(char_to_num('Z'), 26);
+}
+
+/// Convert a number into a letter (A == 1)
 fn num_to_char(n: usize) -> char {
     LETTERS[n - 1]
 }
 
-fn string_to_vec(s: String) -> Vec<char> {
+#[test]
+fn test_num_to_char() {
+    assert_eq!(num_to_char(1), 'A');
+    assert_eq!(num_to_char(26), 'Z');
+}
+/// Convert a string into a vector of characters
+fn string_to_vec(s: &String) -> Vec<char> {
     s.chars().collect::<Vec<char>>()
 }
 
-fn vec_to_string(v: Vec<char>) -> String {
-    v.iter().collect()
+#[test]
+fn test_string_to_vec() {
+    let hello = String::from("HELLO");
+    assert_eq!(string_to_vec(&hello), vec!['H', 'E', 'L', 'L', 'O']);
 }
 
-fn string_to_nums(s: String) -> Vec<usize> {
+// Convert a string into a vector of numbers
+fn string_to_nums(s: &String) -> Vec<usize> {
     string_to_vec(s)
         .into_iter()
         .map(|x| char_to_num(x))
         .collect::<Vec<usize>>()
 }
 
-fn nums_to_string(v: Vec<usize>) -> String {
-    v.into_iter().map(|x| num_to_char(x)).collect::<String>()
+#[test]
+fn test_string_to_nums() {
+    let s = String::from("AZ");
+    let v: Vec<usize> = vec![1, 26];
+    assert_eq!(string_to_nums(&s), v);
 }
 
+/// Convert a vector of numbers into a string
+fn nums_to_string(v: &Vec<usize>) -> String {
+    v.into_iter().map(|&x| num_to_char(x)).collect::<String>()
+}
+
+#[test]
+fn test_nums_to_string() {
+    let n: Vec<usize> = vec![1, 26];
+    let s = String::from("AZ");
+    assert_eq!(nums_to_string(&n), s);
+}
+
+/// Add two equal-length vectors together modulo (something)
 fn modulo_add(a: &Vec<usize>, b: &Vec<usize>, modulus: usize) -> Vec<usize> {
     a.iter()
         .zip(b.iter())
-        .map(|(x, y)| (x + y) % modulus)
+        .map(|(x, y)| {
+            let ans = (x + y) % modulus as usize;
+            match ans {
+                0 => 26,
+                _ => ans,
+            }
+        })
         .collect::<Vec<usize>>()
 }
 
-fn modulo_subtract(a: &Vec<usize>, b: &Vec<usize>, modulus: usize) -> Vec<usize> {
-    a.iter()
-        .zip(b.iter())
-        .map(|(x, y)| (x - y) % modulus)
-        .collect::<Vec<usize>>()
+#[test]
+fn test_modulo_add() {
+    let x: Vec<usize> = vec![25, 26, 1];
+    let y: Vec<usize> = vec![1, 1, 1];
+    assert_eq!(modulo_add(&x, &y, 26), vec![26, 1, 2]);
+}
+
+/// Subtract `b` from `a` modulo `modulus`
+fn modulo_subtract(a: &Vec<usize>, b: &Vec<usize>, modulus: i8) -> Vec<usize> {
+    // Need to convert to signed integers to handle negative numbers
+    let int_a: Vec<i8> = a.into_iter().map(|&i| i as i8).collect();
+    let int_b: Vec<i8> = b.into_iter().map(|&i| i as i8).collect();
+    int_a
+        .iter()
+        .zip(int_b.iter())
+        .map(|(x, y)| {
+            let ans = ((x + modulus - y) % modulus) as usize;
+            match ans {
+                0 => 26,
+                _ => ans,
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test_modulo_subtract() {
+    let x: Vec<usize> = vec![1, 2];
+    let y: Vec<usize> = vec![2, 2];
+    assert_eq!(modulo_subtract(&x, &y, 26), vec![25, 26]);
+}
+
+fn match_key_length(key: &String, length: usize) -> String {
+    let l = key.len();
+    let n_repeats = (length / l) + 1;
+    let mut new_key = key.repeat(n_repeats);
+    new_key.truncate(length);
+    new_key
+}
+
+#[test]
+fn test_match_key_length() {
+    assert_eq!(
+        match_key_length(&String::from("KEY"), 8),
+        String::from("KEYKEYKE")
+    );
+    assert_eq!(match_key_length(&String::from("KEY"), 0), String::from(""));
+}
+
+fn encode(input: &String, key: &String) -> String {
+    let new_key = match_key_length(key, input.len());
+    let input_nums = string_to_nums(&input);
+    let key_nums = string_to_nums(&new_key);
+    let out_nums = modulo_add(&input_nums, &key_nums, 26);
+    nums_to_string(&out_nums)
+}
+
+#[test]
+fn test_encode() {
+    let s = String::from("YZAB");
+    let k = String::from("A");
+    assert_eq!(encode(&s, &k), String::from("ZABC"));
+}
+
+fn decode(input: &String, key: &String) -> String {
+    let new_key = match_key_length(key, input.len());
+    let input_nums = string_to_nums(&input);
+    let key_nums = string_to_nums(&new_key);
+    let out_nums = modulo_subtract(&input_nums, &key_nums, 26);
+    nums_to_string(&out_nums)
+}
+
+#[test]
+fn test_decode() {
+    let text = String::from("ZABC");
+    let key = String::from("A");
+    assert_eq!(decode(&text, &key), String::from("YZAB"));
+}
+
+#[test]
+fn test_encode_decode() {
+    let text = String::from("HELLOWORLD");
+    let key = String::from("KEY");
+    assert_eq!(decode(&encode(&text, &key), &key), text);
 }
 
 fn main() {
-    // let in_file = "poem.txt";
-    // let in_text = read_input(in_file);
-    // println!("Key:\n{}", in_text);
-    // let in_nums = string_to_nums(in_text);
-    // println!("Key nums:\n{:?}", in_nums);
+    let text_file = "poem.txt";
     let key_file = "key.txt";
-    let key_text = read_input(key_file);
-    println!("Key:\n{}", key_text);
-    let key_nums = string_to_nums(key_text);
-    println!("Key nums:\n{:?}", key_nums);
-    let m = modulo_add(&key_nums, &key_nums, 26);
-    println!("Mod add:\n{:?}", m);
-    println!("{}", nums_to_string(m));
+    let text = read_input(&text_file);
+    let key = read_input(&key_file);
+    let encoded = encode(&text, &key);
+    let decoded = decode(&encoded, &key);
+    println!("{:?}", text);
+    println!("{:?}", encoded);
+    println!("{:?}", decoded);
 }
